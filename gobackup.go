9b85daf1b5168833c68edcf60c77132d9d9a887f
Fix <nicholas_fix@intuit.com>
2018-08-23 18:38:30 -0500

    progress bar

--- gobackup.go
+++ gobackup.go
@@ -14,15 +14,11 @@ func main() {
   submain(args)
 }
 
-
-
-
 func sync (folder1 string, folder2 string) {
   err := fsync.Sync(folder2, folder1)
   if err != nil {
     fmt.Println("Error copying folder", err)
-  } else {
-    fmt.Println("Sync Complete")
+    os.Exit(1)
   }
 }
 
@@ -36,36 +32,37 @@ func submain(args []string) {
   folder2 := args[2]
   fmt.Println("Syncing: "+ folder1 +" to: " + folder2)
 
-  //
+
 
   size1, _ := folder.Size(folder1)
   size2, _ := folder.Size(folder2)
-  fmt.Println(size1)
-  fmt.Println(size2)
   difference := math.Abs(float64(size1 - size2))
   progress := difference - math.Abs(float64((size1 - size2)))
 
 
-  fmt.Println(difference)
 
   bar := pb.New(int(difference))
 
   bar.Start()
 
-  go sync(folder1, folder2)
-  
-  for progress < difference {
-
-    size2, _  := folder.Size(folder2)
-    size1, _  := folder.Size(folder1)
-
-    progress = difference - math.Abs(float64(size1 - size2))
-    bar.Set(int(progress))
-  }
 
 
+  go func() {
+    for progress < difference {
+
+      size2, _  := folder.Size(folder2)
+      size1, _  := folder.Size(folder1)
+
+      progress = difference - math.Abs(float64(size1 - size2))
+      bar.Set(int(progress))
+    }
+  }()
 
 
+
+  sync(folder1, folder2)
+
+  bar.Set(int(difference))
   bar.Finish()
 
 
